fe849179978696c1c88cfda4dccb44f9a68e65c7
Eduardo Canche Vazquez <eduardo.942712@gmail.com>
2021-05-13 11:24:50 -0500

    app component removed

--- projects/web-components/src/app/app.module.ts
+++ projects/web-components/src/app/app.module.ts
@@ -1,29 +1,34 @@
-import { Injector, NgModule } from '@angular/core';
+import { DoBootstrap, Injector, NgModule } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PopupComponent } from './popup/popup.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
         PopupComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        BrowserAnimationsModule
     ],
     providers: [],
     entryComponents: [
         PopupComponent
-    ],
-    bootstrap: [ AppComponent ]
+    ]
 })
-export class AppModule {
+export class AppModule implements DoBootstrap {
     constructor(private injector: Injector) {
         // Convert `PopupComponent` to a custom element.
         const PopupElement = createCustomElement(PopupComponent, { injector });
         // Register the custom element with the browser.
         customElements.define('popup-element', PopupElement);
+    }
+
+    ngDoBootstrap(): void {
+        // // Convert `PopupComponent` to a custom element.
+        // const PopupElement = createCustomElement(PopupComponent, { injector: this.injector });
+        // // Register the custom element with the browser.
+        // customElements.define('popup-element', PopupElement);
     }
 }
